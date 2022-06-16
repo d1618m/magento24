cb2a3d3db51ce85198062270d6f63c60827d6df9
d1618m <divya.kgkrunch@gmail.com>
2022-06-16 17:16:23 +0530

    B

---
+++ app/code/Kgkrunch/CategoryAttribute/Setup/InstallData.php
@@ -0,0 +1,45 @@
+<?php
+namespace Kgkrunch\CategoryAttribute\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+
+class InstallData implements InstallDataInterface
+{
+
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Category::ENTITY,
+            'cms_custom_block',
+            [
+                'type' => 'int',
+                'label' => 'Cms Custom Block',
+                'input' => 'select',
+                'sort_order' => 100,
+                'source' => 'Magento\Catalog\Model\Category\Attribute\Source\Page',
+                'global' => 2,
+                'visible' => true,
+                'required' => true,
+                'user_defined' => false,
+                'default' => null,
+                'group' => 'General Information',
+                'backend' => ''
+            ]
+        );
+    }
+}
